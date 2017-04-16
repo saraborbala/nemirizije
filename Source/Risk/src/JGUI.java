8ca8bac8fd8006dbbd83c4b3c8be85d91bf8b4f5
sstrb <straumouse@gmail.com>
2017-04-16 13:24:17 +0200

    New build
    Új build, most elvileg minden működik, és jó fájlok vanna fent GIT-en.

--- Source/Risk/src/JGUI.java
+++ Source/Risk/src/JGUI.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JDesktopPane;
-
+ 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JMenuBar;
--- Source/Risk/src/NewPlayer.java
+++ Source/Risk/src/NewPlayer.java
@@ -4,8 +4,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import org.eclipse.swt.events.PaintEvent;
-
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
@@ -16,8 +14,6 @@ import javax.swing.UIManager;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 
