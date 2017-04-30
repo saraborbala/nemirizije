527ab4f3a89a7514cc302df8677dd17a1ce5154c
nemeskee <nemeskee@sch.bme.hu>
2017-04-30 12:10:58 +0200

    GameState és Command osztályok skeletonja
    Elég gyenge váz, még a szerver oldalról is fogok bele változókat pakolni

---
+++ Source/Risk/src/Command.java
@@ -0,0 +1,7 @@
+public class Command {
+	// Ezen az osztályon keresztül kommunikál a map és az engine. Függvények lesznek benne
+	// pl: Attack(player 1, player 2) ... 
+	
+	
+
+}
---
+++ Source/Risk/src/GameState.java
@@ -0,0 +1,14 @@
+
+public class GameState {
+	// Csak publikus osztályváltozók, ide jön minden ami szükséges arra, hogy a GUI felépüljön, illetve
+	// néhány szerver mûködéséshez fontos dolog
+	
+	// public Attack attack;
+	// public Map map;
+	// stb. kérlek írjátok be mi kell még
+	
+	
+	public int connected = 0;
+	public int isGameOver = 0;
+
+}
